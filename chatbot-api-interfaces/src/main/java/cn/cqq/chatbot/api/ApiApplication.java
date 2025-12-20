9ded289951e52bb6f6338d68c195a2c89697629e
chenqingquan <chenqingquan@itg.com.cn>
2025-12-20 23:11:37 +0800

    feat: init project

---
+++ chatbot-api-interfaces/src/main/java/cn/cqq/chatbot/api/ApiApplication.java
@@ -0,0 +1,17 @@
+package cn.cqq.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: chenqingquan
+ * @description: 启动类
+ * @date: 2025/12/20 22:39
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/cn/cqq/chatbot/api/test/Apitest.java
@@ -0,0 +1,9 @@
+package cn.cqq.chatbot.api.test;
+
+/**
+ * @author: chenqingquan
+ * @description: 单元测试
+ * @date: 2025/12/20 23:00
+ */
+public class Apitest {
+}
